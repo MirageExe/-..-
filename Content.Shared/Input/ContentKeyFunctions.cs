8c31cd8f627f4eec53ba1edf1930ac2159fd305d
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-12-24 19:16:44 +0300

    [Port] Posing System (#216)
    Co-authored-by: _kote <143940725+FaDeOkno@users.noreply.github.com>

--- Content.Shared/Input/ContentKeyFunctions.cs
+++ Content.Shared/Input/ContentKeyFunctions.cs
@@ -189,6 +189,15 @@ namespace Content.Shared.Input
         public static readonly BoundKeyFunction ResetZoom = "ResetZoom";
         public static readonly BoundKeyFunction ToggleStanding = "ToggleStanding"; // WD EDIT
         public static readonly BoundKeyFunction ResistGrab = "ResistGrab"; // Goobstation - Grab Intent
+        // Orion-Start
+        public static readonly BoundKeyFunction TogglePosing = "TogglePosing";
+        public static readonly BoundKeyFunction PosingOffsetLeft = "PosingOffsetLeft";
+        public static readonly BoundKeyFunction PosingOffsetRight = "PosingOffsetRight";
+        public static readonly BoundKeyFunction PosingOffsetUp = "PosingOffsetUp";
+        public static readonly BoundKeyFunction PosingOffsetDown = "PosingOffsetDown";
+        public static readonly BoundKeyFunction PosingRotateNegative = "PosingRotateNegative";
+        public static readonly BoundKeyFunction PosingRotatePositive = "PosingRotatePositive";
+        // Orion-End
 
         // Shitmed Change Start
         public static readonly BoundKeyFunction TargetHead = "TargetHead";
